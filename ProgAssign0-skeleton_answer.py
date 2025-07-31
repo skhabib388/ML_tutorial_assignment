1192d93a5fa9bead5fcdce9403caa4d3aa195316
Sk Habibullah <69055466+skhabib388@users.noreply.github.com>
2025-08-01 00:05:40 +0530

    Add files via upload

---
+++ ProgAssign0-skeleton_answer.py
@@ -0,0 +1,40 @@
+## Programming Assignment 0
+## Practice question for automatic grading
+
+## Do not use any additional libraries
+import sys
+
+
+## Problem: Write a function that takes a list of integers as
+## and returns the sum of all the integers in the list.
+
+def sum_of_list(lst):
+    ## Remove the pass statement and write your code here
+    ## CODEBASE:S01 Start
+    sum_tot = 0
+    for num in lst:
+        sum_tot += num
+    return sum_tot
+
+    ## CODEBASE:S01 End
+
+ 
+
+
+## Skeleton code for testing
+## DO NOT MODIFY, Please maintain this structure
+if __name__ == '__main__':
+
+    """
+    ALERT: * * * No changes are allowed in this section  * * *
+    """
+
+    numbers_str = sys.argv[1:]  
+    numbers = [int(num) for num in numbers_str]
+
+    # print("List of numbers:", numbers)
+    print(sum_of_list(numbers)) 
+    
+    """ End to call """
+
+
